6ed9ff4b52884ac95c6ea543b16f5e598a201351
l.randegger <l.randegger@altis.ch>
2020-02-09 17:25:30 +0100

    leerer Scanner

---
+++ IpScanner.java
@@ -0,0 +1,12 @@
+import java.awt.event.ActionEvent;
+
+/**
+ * IpScanner
+ */
+public class IpScanner extends SimplifiedListener{
+
+    public void actionPerformed(ActionEvent e){
+        // da drin passiert alles wo scannt
+        
+    }
+}
